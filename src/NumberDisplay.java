cf94555c07b63cbd98a38df8e05a99f5c8aaa14a
hampus0151 <hampus.nasvall@gmail.com>
2019-11-05 11:14:22 +0100

    testprogram till examinerande uppgifter

--- src/NumberDisplay.java
+++ src/NumberDisplay.java
@@ -1,3 +1,33 @@
 public class NumberDisplay {
-    
+    protected int limit;
+    protected int value;
+
+    public NumberDisplay(int maxLimit) {
+        value = 0;
+        limit = maxLimit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int newValue) {
+        this.value = newValue;
+        if (newValue < limit && newValue > 0) {
+
+        }
+    }
+
+    public String getDisplayValue() {
+        String strValue = Integer.toString(value);
+        return strValue;
+    }
+
+    public int increment() {
+        value++;
+        if (value >= limit) {
+            value = 0;
+        }
+        return value;
+    }
 }
---
+++ src/TestNumberDisplay.java
@@ -0,0 +1,8 @@
+public class TestNumberDisplay {
+
+    public static void main (String[] args) {
+        NumberDisplay n1 = new NumberDisplay(60);
+        System.out.println(n1.value);
+    }
+
+}
