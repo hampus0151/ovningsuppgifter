c3ea93aea1019b047dc225178987faa316f19fa6
hampus0151 <hampus.nasvall@gmail.com>
2019-11-19 10:34:42 +0100

    comments added

--- src/NumberDisplay.java
+++ src/NumberDisplay.java
@@ -2,21 +2,37 @@ public class NumberDisplay {
     protected int limit;
     protected int value;
 
+    /**
+     * Sets a value for numberdisplay.
+     * @param maxLimit defines a limit for the numberdisplay.
+     */
     public NumberDisplay(int maxLimit) {
         value = 0;
         limit = maxLimit;
     }
 
+    /**
+     * returnes value when getValue is called
+     * @return
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * changes value of int value when its reaches the defined limit
+     * @param newValue
+     */
     public void setValue(int newValue) {
         if (newValue < limit && newValue > 0) {
             this.value = newValue;
         }
     }
 
+    /**
+     * gives getDispayValue an value to display and changes the value to ex 01 if the value is less than 10.
+     * @returns the value for display
+     */
     public String getDisplayValue() {
         String display = null;
         if (value > 9) {
@@ -27,6 +43,10 @@ public class NumberDisplay {
         return display;
     }
 
+    /**
+     *
+     * @return
+     */
     public int increment() {
         value++;
         if (value >= limit) {
